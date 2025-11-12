9639d1d6be44c4f2d0d59512bbefdf27cadff9a1
shivamsavaliya <savaliyashivam2002@gmail.com>
2025-11-11 21:49:07 -0600

    Initial commit

---
+++ src/main/java/com/finalproject/HotProperties/HotPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.HotProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/HotProperties/HotPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.HotProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
